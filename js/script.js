89113fc79c160fa4f50a1168d76f000789d15270
Amanda <amanda770328@gmail.com>
2024-04-24 14:37:45 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,41 +1,5 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 'auto',
-    spaceBetween: 250,
-    breakpoints: {
-        1440: {
-            spaceBetween: 230,
-        },
-        992: {
-            spaceBetween: 200,
-        },
-        768: {
-            spaceBetween: 120,
-        },
-        480: {
-            spaceBetween: 110,
-        },
-        360: {
-            spaceBetween: 60,
-        },
-    },   
-    centeredSlides: true,
-    grabCursor: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+/*global $,document,window */
 
-function scrollNav() {
-$('.dots a').click(function(){
-    $(".active").removeClass("active");     
-    $(this).addClass("active");
-    
-    $('html, body').stop().animate({
-    scrollTop: $($(this).attr('href')).offset().top - 160
-    }, 500);
-    return false;
+$(document).ready(function () {
+	new WOW().init();
 });
-}
-scrollNav();
